08fbdc0457e345e84a03aae850c36caf83b30646
Waqar-Ul-Hassan <118990135+Waqar-Ul-Hassan@users.noreply.github.com>
2025-10-05 12:26:20 -0400

    Time: 9 ms (28.51%) | Memory: 24.2 MB (13.70%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,21 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = {}
-
+        freq = {}
         for n in nums:
-            if n not in counter:
-                counter[n] = 1
-            else:
-                counter[n] += 1
-            print(counter[n])
+            freq[n] = freq.get(n, 0) + 1
         
-
-        vals = sorted(counter.values(), reverse=False)[len(counter)-k:]
-        keys = [key for key, val in counter.items() if val in vals]
-        return keys
-
-
-
-
-
+        # Step 2: bucket sort by frequency
+        buckets = [[] for _ in range(len(nums) + 1)]
+        for num, count in freq.items():
+            buckets[count].append(num)
         
+        # Step 3: gather from high freq down
+        res = []
+        for i in range(len(buckets) - 1, 0, -1):
+            for num in buckets[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
